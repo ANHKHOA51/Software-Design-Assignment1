ca9618ac58a7319a3a2f8070691307c540879052
hlam3004 <hlam.30042005@gmail.com>
2026-02-23 16:29:40 +0700

    update file user.model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -13,18 +13,15 @@ export function findById(id) {
 export function loadAllUsers() {
   return db('users').orderBy('id', 'desc');
 }
-
 export function findUsersByRole(role) {
   return db('users')
     .select('users.id', 'users.fullname', 'users.email', 'users.role')
     .where('users.role', role)
     .orderBy('users.fullname', 'asc');
 }
-
 export function findByUserName(username) {
   return db('users').where('username', username).first();
 }
-
 export async function update(id, user) {
   // SỬA: Thêm await
   const rows = await db('users')
@@ -34,9 +31,23 @@ export async function update(id, user) {
 
   return rows[0];
 }
-
 export function findByEmail(email) {
   return db('users').where('email', email).first();
+}
+export async function deleteUser(id) {
+  return db('users')
+    .where('id', id)
+    .del();
+}
+export function updateUserInfo(user_id, { email, fullname, address }) {
+  return db('users')
+    .where('id', user_id)
+    .update({ email, fullname, address });
+}
+export function updateUserRole(user_id, role) {
+  return db('users')
+    .where('id', user_id)
+    .update({ role: role, is_upgrade_pending: false });
 }
 
 // Verify email user
@@ -45,24 +56,9 @@ export function verifyUserEmail(user_id) {
     .where('id', user_id)
     .update({ email_verified: true });
 }
-export function updateUserInfo(user_id, { email, fullname, address }) {
-  return db('users')
-    .where('id', user_id)
-    .update({ email, fullname, address });
-}
+
 export function markUpgradePending(user_id) {
   return db('users')
     .where('id', user_id)
     .update({ is_upgrade_pending: true });
-}
-export function updateUserRole(user_id, role) {
-  return db('users')
-    .where('id', user_id)
-    .update({ role: role, is_upgrade_pending: false });
-}
-
-export async function deleteUser(id) {
-  return db('users')
-    .where('id', id)
-    .del();
 }
